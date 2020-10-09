68d8a31b3fb969da985e49eb29dbee52f94b349e
ophelia swen <ophelia.swen@gmail.com>
2020-10-09 13:36:54 -0700

    write out method and write out test

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -4,7 +4,26 @@ class Volunteer
 
   def initialize(attributes)
     @name = attributes[:name]
-    @project_id = attributes[:project_id]
     @id = attributes[:id]
+    @project_id = attributes[:project_id]
   end
-end
+
+  def ==(another_volunteer)
+    self.name().==(another_volunteer()).&(self.project_id().==(another_volunteer.project_id())).&(self.id().==(another_volunteer.id()))
+  end
+
+  def self.all
+    returned_volunteers = DB.exec("SELECT* from volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+     name = volunteer.fetch("name")
+     project_id = volunteer.fetch("project_id").to_i
+     id = volunteer.fetch("id").to_i
+     volunteers.push(Volunteer.new(:name => name, :project_id => project_id, :id => id))
+    end
+    volunteers
+  end
+
+  def save
+    saved_volunteer = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}, ;)
+
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,8 +4,10 @@ require "spec_helper"
 describe(Project) do
   describe("#initialize") do
     it("is initialized with name and id") do
-      project1 = Project.new({:name => "Project Spruce", :id => nil})
-      expect(project1.name()).to(eq("Project Spruce"))
+      project_one = Project.new({:name => "Project Spruce", :id => nil})
+      expect(project_one.name()).to(eq("Project Spruce"))
     end
   end
+
+  
 end
